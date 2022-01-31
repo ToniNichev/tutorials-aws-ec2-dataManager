df55fd455a917cc2ccfe8dd19aa56003ff3bd188
ToniNichev <toni.nichev@gmail.com>
2022-01-31 11:40:30 -0500

    fixing small issue when you delete all items

--- src/components/AddFlagPopup/index.js
+++ src/components/AddFlagPopup/index.js
@@ -38,7 +38,7 @@ const addEditFlag = async (editFlagId, closePopup) => {
 
 
 const Renderer = ({flags, closePopup, editFlagId}) => {  
-  let conceptId = parseInt(flags[flags.length - 1].conceptId) + 1;
+  let conceptId = typeof flags ? 0 : parseInt(flags[flags.length - 1].conceptId) + 1;
   let displayName = '';
   let description = '';
   let parentIds = '';
